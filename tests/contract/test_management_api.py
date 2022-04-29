21cd745fe28d0b11cb0fcd748c91795a83f64333
Haseeb Majid <hello@haseebmajid.dev>
2022-04-29 18:22:05 +0000

    feat: add more contract tests
    Add contract tests for question api of the management api
    
    Fixes: #51

--- tests/contract/test_management_api.py
+++ tests/contract/test_management_api.py
@@ -1,14 +1,16 @@
 import asyncio
 import atexit
-import os
 import time
 from http import HTTPStatus
 
 import pytest
-from pactman import Consumer, Like, Provider
+from pactman import Consumer, EachLike, Like, Provider, Term
 
 from app.clients.management_api.api.games_api import AsyncGamesApi
+from app.clients.management_api.api.questions_api import AsyncQuestionsApi
 from app.clients.management_api.exceptions import UnexpectedResponse
+from app.core.config import get_settings
+from app.game_state.game_state_factory import get_question_api
 from app.room.room_factory import get_game_api
 
 
@@ -21,6 +23,10 @@ def event_loop():
 
 @pytest.fixture(scope="session")
 def pact():
+    settings = get_settings()
+    settings.MANAGEMENT_API_URL = "http://localhost"
+    settings.MANAGEMENT_API_PORT = 1234
+
     pact = Consumer("core_api").has_pact_with(Provider("management_api"), port=1234, use_mocking_server=True)
     pact.start_mocking()
     time.sleep(0.25)
@@ -30,10 +36,14 @@ def pact():
 
 @pytest.fixture(scope="session")
 def game_api_client() -> AsyncGamesApi:
-    os.environ["BANTER_BUS_CORE_API_MANAGEMENT_API_URL"] = "http://localhost"
-    os.environ["BANTER_BUS_CORE_API_MANAGEMENT_API_PORT"] = "1234"
     game_api = get_game_api()
     return game_api
+
+
+@pytest.fixture(scope="session")
+def question_api_client() -> AsyncQuestionsApi:
+    question_api = get_question_api()
+    return question_api
 
 
 @pytest.mark.asyncio
@@ -89,3 +99,69 @@ async def test_not_found_game(pact, game_api_client: AsyncGamesApi):
     with pact:
         with pytest.raises(UnexpectedResponse):
             await game_api_client.get_game(game_name="quibly2")
+
+
+@pytest.mark.asyncio
+async def test_get_random_groups(pact, question_api_client: AsyncQuestionsApi):
+    expected = {"groups": EachLike("animal_group", minimum=3)}
+
+    pact.given("3 random groups exist for game fibbing_it and round opinion").upon_receiving(
+        "a request for getting 3 random groups for game fibbing_it and round opinion"
+    ).with_request(
+        "GET", "/game/fibbing_it/question/group:random", query={"round": "opinion", "limit": "3"}
+    ).will_respond_with(
+        HTTPStatus.OK, body=expected
+    )
+
+    with pact:
+        await question_api_client.get_random_groups(game_name="fibbing_it", round="opinion", limit=3)
+
+
+@pytest.mark.asyncio
+async def test_get_question_from_group(pact, question_api_client: AsyncQuestionsApi):
+    expected = EachLike(
+        {
+            "content": Like("Disagree"),
+            "question_id": Like("03a462ba-f483-4726-aeaf-b8b6b03ce3aa"),
+            "type": Term("answer|question", "type"),
+        },
+        minimum=3,
+    )
+
+    pact.given("fibbing_it exists and a group called animal_group on round opinion").upon_receiving(
+        "a request for getting 3 random questions"
+    ).with_request(
+        "GET",
+        "/game/fibbing_it/question:random",
+        query={"round": "opinion", "group_name": "animal_group"},
+    ).will_respond_with(
+        HTTPStatus.OK, body=expected
+    )
+
+    with pact:
+        await question_api_client.get_random_questions(
+            game_name="fibbing_it", round="opinion", group_name="animal_group"
+        )
+
+
+@pytest.mark.asyncio
+async def test_get_question_not_from_group(pact, question_api_client: AsyncQuestionsApi):
+    expected = EachLike(
+        {
+            "content": "to eat ice-cream from the tub",
+            "question_id": "d6318b0d-29e1-4f10-b6a7-37a648364ca6",
+            "type": "question",
+        },
+        minimum=3,
+    )
+
+    pact.given("fibbing_it exists and a round likely").upon_receiving(
+        "a request for getting 3 random questions"
+    ).with_request(
+        "GET", "/game/fibbing_it/question:random", query={"round": "likely", "limit": "3"}
+    ).will_respond_with(
+        HTTPStatus.OK, body=expected
+    )
+
+    with pact:
+        await question_api_client.get_random_questions(game_name="fibbing_it", round="likely", limit=3)
